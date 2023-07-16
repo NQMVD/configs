abf6bda985ea741c4697fec0b919535f6b5a6cd6
ItsNMB <99403507+ItsNMB@users.noreply.github.com>
2023-07-16 20:49:58 +0200

    Initial Upload

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
+    ["<leader>dus"] = {
+      function()
+
+      end
+    }
+  }
+}
